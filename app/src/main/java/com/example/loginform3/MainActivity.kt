4150122336df299566c768ac5cd0048ddf172197
Kasidhad <kkasidhad@gmail.com>
2025-02-12 04:33:37 +0700

    Updated

--- app/src/main/java/com/example/loginform3/MainActivity.kt
+++ app/src/main/java/com/example/loginform3/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.loginform3.ui.theme.LoginForm3Theme
 
